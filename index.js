eecca9599d575f6c46a4ea916fb11dfebdac88ab
fevagg <paingrille19@gmail.com>
2024-11-23 21:05:28 -0300

    first commit since a long time

---
+++ index.js
@@ -0,0 +1,17 @@
+import e from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import { configDotenv } from "dotenv";
+configDotenv({
+  path: ".env",
+});
+
+const port = process.env.port;
+
+const app = e();
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.listen(port, () => {
+  console.log(`Listen on port ${port}`);
+});
